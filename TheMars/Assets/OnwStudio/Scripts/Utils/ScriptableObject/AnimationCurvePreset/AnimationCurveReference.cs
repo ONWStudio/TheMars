11e33de9586da28df2146fbf164801dd23af6161
jeomseon0516 <126733377+jeomseon0516@users.noreply.github.com>
2024-07-31 01:35:29 +0900

    24.07.31 새벽

--- TheMars/Assets/OnwStudio/Scripts/Utils/ScriptableObject/AnimationCurvePreset/AnimationCurveReference.cs
+++ TheMars/Assets/OnwStudio/Scripts/Utils/ScriptableObject/AnimationCurvePreset/AnimationCurveReference.cs
@@ -8,14 +8,5 @@ namespace Onw.ScritableObjects
     public sealed class AnimationCurveReference : ScriptableObject
     {
         [field: SerializeField] public AnimationCurve Curve { get; private set; } = null;
-
-        private void OnValidate()
-        {
-            for (int i = 0; i < Curve.keys.Length; i++)
-            {
-                Debug.Log("in : " + Curve.keys[i].inTangent);
-                Debug.Log("out : " + Curve.keys[i].outTangent);
-            }
-        }
     }
 }
